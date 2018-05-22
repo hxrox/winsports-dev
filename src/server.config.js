5a090933089777568e40d4be2d55a4dd2e2193fb
Moisés Alberto Alemán Macías <moises.aleman@logrand.com>
2018-05-21 21:15:59 -0500

    update build

--- src/server.config.js
+++ src/server.config.js
@@ -9,7 +9,6 @@ const config = {
   },
   DATABASE: {
     MONGODB_STRING_CONNECTION: process.env.MONGODB_URI || 'mongodb://localhost:27017/winsports'
-    // MONGODB_STRING_CONNECTION: process.env.MONGODB_URI || 'mongodb://heroku_k9w08kc2:u9jbesf6vsm15nbhlkaqedm6nm@ds147884.mlab.com:47884/heroku_k9w08kc2'
   },
   EMAIL: {
     HOST: 'mail.winsports.mx',
